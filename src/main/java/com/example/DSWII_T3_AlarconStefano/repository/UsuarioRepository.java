4dbf7ea653e8fba4b2bc339d65c6e5d861c42eec
SirHotcake <96447228+SirHotcake@users.noreply.github.com>
2024-07-02 22:39:15 -0500

    pregunta 4 commit 2 UsuarioRepository.java

--- src/main/java/com/example/DSWII_T3_AlarconStefano/repository/UsuarioRepository.java
+++ src/main/java/com/example/DSWII_T3_AlarconStefano/repository/UsuarioRepository.java
@@ -4,7 +4,7 @@ import com.example.DSWII_T3_AlarconStefano.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-
+//
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     Optional<Usuario> findByNomUsuario(String nomUsuario);
 }
