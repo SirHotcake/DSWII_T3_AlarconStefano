324d222811e2cd0435aed8e40dad94fb226d5369
SirHotcake <96447228+SirHotcake@users.noreply.github.com>
2024-07-02 22:38:18 -0500

    pregunta 3 commit 2 PublicationController.java

--- src/main/java/com/example/DSWII_T3_AlarconStefano/controller/PublicationController.java
+++ src/main/java/com/example/DSWII_T3_AlarconStefano/controller/PublicationController.java
@@ -16,7 +16,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/pubs-dto")
 public class PublicationController {
-
+//
     @Autowired
     private PublicacionRepository publicacionRepository;
 
